1d90cd916f0e1ebd84df3a1362d0ad9ab56a696d
園田 <e184223sk@kurume-it.ac.jp>
2021-07-17 19:13:51 +0900

    VRのアセットとVRInputとか

---
+++ Assets/snd/Scripts/SpeedUPAbi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class SpeedUPAbi :AbilitySuper
+{
+    private float _originSpeed;
+
+
+    public SpeedUPAbi(float cooltime = 10f, float usabletime = 5f)
+    {
+        _coolTime = cooltime;
+        _usableTime = usabletime;
+    }
+
+    public override void Effect(PlayerCtrler player)
+    {
+        
+
+        _originSpeed = _player._speed;
+         player._speed *= 5.0f;
+
+    }
+
+    public override void AntiEffect(PlayerCtrler player)
+    {
+         player._speed = _originSpeed;
+
+    }
+}
+
