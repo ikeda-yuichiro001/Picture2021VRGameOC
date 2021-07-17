ef54188cb944f5faac911c8474243fd654c5cb98
e194140nh <e194140nh@kurume-it.ac.jp>
2021-07-17 16:40:32 +0900

    ちょっと躓いてる

--- Assets/nisimura/script/AutoGetCompornennt.cs
+++ Assets/nisimura/script/AutoGetCompornennt.cs
@@ -8,12 +8,14 @@ public class AutoGetCompornennt : Editor
 {
     public override void OnInspectorGUI()
     {
+        GameObject game = GameObject.Find("BreakObject");
         //元のInspector部分を表示
         base.OnInspectorGUI();
 
         //ボタンを表示
         if (GUILayout.Button("Button"))
         {
+            //game = 
             Debug.Log("押した!");
         }
     }
