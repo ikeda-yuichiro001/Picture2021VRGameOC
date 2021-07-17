67ff550c713917d41ad041d5b6860e76ad8a96a2
e194140nh <e194140nh@kurume-it.ac.jp>
2021-07-17 14:24:50 +0900

    建物実装したら自動的にrigidbodyと破壊処理が割り振られていくように制作中

---
+++ Assets/nisimura/script/AutoGetCompornennt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AutoGetCompornennt))]//拡張するクラスを指定
+public class AutoGetCompornennt : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //元のInspector部分を表示
+        base.OnInspectorGUI();
+
+        //ボタンを表示
+        if (GUILayout.Button("Button"))
+        {
+            Debug.Log("押した!");
+        }
+    }
+}
